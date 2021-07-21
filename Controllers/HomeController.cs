fa5db561a1e95f969628a7d87d5d9dfdbfd2cc0e
rajadev2019 <47190156+rajadev2019@users.noreply.github.com>
2021-07-21 21:14:32 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace dotnet_docker.Controllers
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger
+            _logger = logger;
         }
 
         public IActionResult Index()
